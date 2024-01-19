8bc61908e64ceb243b4dc338081309b72155b0b0
URIAN VIERA <urian1213viera@gmail.com>
2024-01-19 09:43:07 -0500

    Update cuenta_creada_email.php

--- emails/cuenta_creada_email.php
+++ emails/cuenta_creada_email.php
@@ -49,7 +49,7 @@ try {
     $mail->Body .= "<p>En <strong style='color:#ff6d0c;'>Parking</strong> le damos la bienvenida y estamos encantados de tenerte como nuestro cliente.</p>";
     $mail->Body .= "<p>Tu cuenta se ha creado exitosamente.</p>";
     $mail->Body .= "<p>Para acceder con tu cuenta a la plataforma, haga clic en el siguiente enlace:</p><br>";
-    $mail->Body .= "<a href='https://alcvaletparking.com/app/' style='background: #ff6d0c; font-size:15px; padding: 10px 20px; border-radius: 25px;text-decoration: unset; color:#fff;'>Acceder Ahora</a>";
+    $mail->Body .= "<a href='https://parking.com/app/' style='background: #ff6d0c; font-size:15px; padding: 10px 20px; border-radius: 25px;text-decoration: unset; color:#fff;'>Acceder Ahora</a>";
     $mail->Body .= "</section>";
 
     $mail->Body .= "<section style='margin-top: 50px; margin-bottom: 70px; font-size: 18px; line-height: 7px;'>";
@@ -57,9 +57,6 @@ try {
     $mail->Body .= "<p>Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos.</p>";
     $mail->Body .= '<p>¡Esperamos que tengas una experiencia increíble!</p>';
     $mail->Body .= "</section>";
-
-    $mail->Body .= "<a href='https://alcvaletparking.com'><img src='https://alcvaletparking.com/app/assets/custom/imgs/logo.png' alt='Parking' style='width: 100%; max-width: 100px; height: auto; display: block; float: left; margin-top: 40px; border-radius: 5px;' /></a>";
-
 
     //Copia de envio del email
     $headers = 'From: Parking <info@alcvaletparking.com>' . "\r\n";
