2fe3347efd050f251c53775f577152e88ef032e9
URIAN VIERA <urian1213viera@gmail.com>
2024-01-19 09:42:02 -0500

    Update factura_email.php

--- emails/factura_email.php
+++ emails/factura_email.php
@@ -52,14 +52,13 @@ try {
     $mail->Body .= "<section style='margin-top: 10px; font-size: 18px; line-height: 7px;'>";
     $mail->Body .= "<p>En <strong style='color:#ff6d0c;'>Parking</strong> le damos la bienvenida y estamos encantados de tenerte como nuestro cliente.</p>";
     $mail->Body .= "<p>Para descargar tu factura solo debes hacer clic en el siguiente enlace:</p><br><br>";
-    $mail->Body .= "<a href='https://alcvaletparking.com/app/dashboard/FacturaClientePDF.php?idReserva=" . $IdReserva . " ' style='background: #ff6d0c; font-size:15px; padding: 10px 20px; border-radius: 25px;text-decoration: unset; color:#fff;'>Descargar Factura</a> <br><br><br>";
+    $mail->Body .= "<a href='https://parking.com/app/dashboard/FacturaClientePDF.php?idReserva=" . $IdReserva . " ' style='background: #ff6d0c; font-size:15px; padding: 10px 20px; border-radius: 25px;text-decoration: unset; color:#fff;'>Descargar Factura</a> <br><br><br>";
     $mail->Body .= "</section>";
 
 
     $mail->Body .= "<section style='margin-top: 50px; margin-bottom: 70px; font-size: 18px; line-height: 7px;'>";
     $mail->Body .= "<p>Gracias de nuevo por elegir <strong style='color:#ff6d0c;'>Parking</strong>.</p>";
     $mail->Body .= "<p>Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos.</p>";
-    $mail->Body .= "<a href='https://alcvaletparking.com'><img src='https://alcvaletparking.com/app/assets/custom/imgs/logo.png' alt='Parking' style='width: 100%; max-width: 100px; height: auto; display: block; float: left; margin-top: 40px; border-radius: 5px;' /></a>";
     $mail->AltBody = '<p>¡Esperamos que tengas una experiencia increíble!</p>';
     $mail->Body .= "</section>";
 
